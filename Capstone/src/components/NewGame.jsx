f96f7901e648dc0cfe60078b90b3484fd43af183
root <root@DESKTOP-FMI1SH6>
2023-05-22 12:31:08 -0400

    new game

--- Capstone/src/components/NewGame.jsx
+++ Capstone/src/components/NewGame.jsx
@@ -6,12 +6,30 @@ function NewGame() {
   const returnBack = () => {
     navigate("/");
   };
+  const beginAdventure = () => {
+    navigate("/game");
+  };
   return (
     <>
       <button className="button" onClick={returnBack}>
         Return
       </button>
-      <h2 className="title">Create A Username</h2>
+      <div className="placementTitle">
+        <h2 className="title">Create A Username</h2>
+      </div>
+      <div className="placement">
+        <input
+          id="userTextBox"
+          type="text"
+          name="Username"
+          placeholder="Enter Username"
+        />
+      </div>
+      <div className="placement">
+        <button className="button" onClick={beginAdventure}>
+          Begin Adventure
+        </button>
+      </div>
     </>
   );
 }
