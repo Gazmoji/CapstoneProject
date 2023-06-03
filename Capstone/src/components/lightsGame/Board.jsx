1ec414bfed46be0e78738d272ca79b739d60b46b
root <root@DESKTOP-FMI1SH6>
2023-06-03 15:00:32 -0400

    css

--- Capstone/src/components/lightsGame/Board.jsx
+++ Capstone/src/components/lightsGame/Board.jsx
@@ -1,164 +1,269 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonRunning } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux'
-import '../../styles/styles.css';
-import { upScore } from '../../store/slices/userSlice';
+import { useDispatch } from "react-redux";
+import "../../styles/styles.css";
+import { upScore } from "../../store/slices/userSlice";
 
 const Board = () => {
-    const [board, setBoard] = useState(null);
-    const [difficulty, setDifficulty] = useState(null);
-    const [gameOver, setGameOver] = useState(false);
-    const [message, setMessage] = useState('');
-    const [isInitialized, setIsInitialized] = useState(false);
-    const [moves, setMoves] = useState(0)
-    const [score, setScore] = useState(0)
+  document.body.className = "spaceBackground";
+  const [board, setBoard] = useState(null);
+  const [difficulty, setDifficulty] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [message, setMessage] = useState("");
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [moves, setMoves] = useState(0);
+  const [score, setScore] = useState(0);
+  const [audioEnabled, setAudioEnabled] = useState(true);
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const toggle = (r, c) => {
-        if (isInitialized && !gameOver) {
-            setBoard(prevBoard => {
-                const newBoard = prevBoard.map(row => [...row]);
+  useEffect(() => {
+    // Code related to audio playback
+    const audio = new Audio(new URL("./SpaceRoute.mp3", import.meta.url));
+    audio.loop = true;
 
-                // Toggle self
-                newBoard[r][c] = newBoard[r][c] === 0 ? 1 : 0;
+    if (audioEnabled) {
+      audio.play().catch((error) => {
+        console.error("Failed to play audio:", error);
+      });
+    } else {
+      audio.pause();
+    }
 
-                // Toggle up
-                if (newBoard[r - 1] !== undefined) {
-                    newBoard[r - 1][c] = newBoard[r - 1][c] === 0 ? 1 : 0;
-                }
-
-                // Toggle right
-                if (newBoard[r][c + 1] !== undefined) {
-                    newBoard[r][c + 1] = newBoard[r][c + 1] === 0 ? 1 : 0;
-                }
-
-                // Toggle down
-                if (newBoard[r + 1] !== undefined) {
-                    newBoard[r + 1][c] = newBoard[r + 1][c] === 0 ? 1 : 0;
-                }
-
-                // Toggle left
-                if (newBoard[r][c - 1] !== undefined) {
-                    newBoard[r][c - 1] = newBoard[r][c - 1] === 0 ? 1 : 0;
-                }
-
-                return newBoard;
-            });
-
-            setMoves((prevMoves) => prevMoves + 1)
-
-            if (moves < 50) {
-                setScore(1000)
-            } else if (moves < 100) {
-                setScore(750) 
-            } else if (moves < 150) {
-                setScore(500)
-            } else if (moves < 200) {
-                setScore(250)
-            }
-        } else {
-            setMessage('Game over. Please start a new game.');
-        }
+    return () => {
+      audio.pause();
     };
+  }, [audioEnabled]);
 
-    const finishGame = () => {
-        dispatch(upScore(score))
-        navigate('/endingScreen');
-    };
+  const toggle = (r, c) => {
+    if (isInitialized && !gameOver) {
+      setBoard((prevBoard) => {
+        const newBoard = prevBoard.map((row) => [...row]);
 
-    // Check if board is complete (no zeroes)
-    const boardComplete = () => {
-        return board.every(row => row.every(cell => cell === 1));
-    };
+        // Toggle self
+        newBoard[r][c] = newBoard[r][c] === 0 ? 1 : 0;
 
-    useEffect(() => {
-        if (!isInitialized) {
-            const easyBoard = [
-                [[1, 0, 0, 0, 1], [0, 1, 0, 1, 0], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [1, 0, 0, 0, 1]],
-                [[0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [1, 0, 1, 0, 1], [0, 1, 0, 1, 0], [0, 0, 1, 0, 0]],
-                [[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 0, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]],
-                [[1, 0, 1, 0, 1], [0, 1, 0, 1, 0], [1, 0, 1, 0, 1], [0, 1, 0, 1, 0], [1, 0, 1, 0, 1]],
-                [[0, 1, 0, 1, 0], [1, 0, 0, 0, 1], [0, 0, 1, 0, 0], [1, 0, 0, 0, 1], [0, 1, 0, 1, 0]]
-            ];
-
-            const randomIndex = Math.floor(Math.random() * easyBoard.length);
-            const randomBoard = easyBoard[randomIndex];
-
-            setBoard(randomBoard);
-            setDifficulty('easy');
-            setGameOver(false);
-            setMessage('');
-            setIsInitialized(true);
+        // Toggle up
+        if (newBoard[r - 1] !== undefined) {
+          newBoard[r - 1][c] = newBoard[r - 1][c] === 0 ? 1 : 0;
         }
-    }, [isInitialized]);
 
-    useEffect(() => {
-        if (board) {
-            if (boardComplete()) {
-                setGameOver(true);
-                setMessage('You win!');
-            }
+        // Toggle right
+        if (newBoard[r][c + 1] !== undefined) {
+          newBoard[r][c + 1] = newBoard[r][c + 1] === 0 ? 1 : 0;
         }
-    }, [board]);
 
-    return (
-        <div>
-            {board && (
-                <>
-                    <table>
-                        <thead></thead>
-                        <tbody>
-                            {board.map((row, i) => (
-                                <Row key={i} row={row} rowIndex={i} gameOver={gameOver} toggle={toggle} difficulty={difficulty} />
-                                ))}
-                        </tbody>
-                    </table>
-                    {gameOver && (
-                        <div className='buttonDiv'>
-                            <p className='message'>Congratulations!</p>
-                            <button className="escape" onClick={finishGame}>
-                                Escape
-                                <FontAwesomeIcon icon={faPersonRunning} />
-                            </button>
-                        </div>
-                    )}
-                </>
-            )}
+        // Toggle down
+        if (newBoard[r + 1] !== undefined) {
+          newBoard[r + 1][c] = newBoard[r + 1][c] === 0 ? 1 : 0;
+        }
 
-            <p className='moves'>Moves: {moves}</p>
-            <p className='dialogBox2'>The object of this game is to set the entire board to blue.</p>
-        </div>
-    );
+        // Toggle left
+        if (newBoard[r][c - 1] !== undefined) {
+          newBoard[r][c - 1] = newBoard[r][c - 1] === 0 ? 1 : 0;
+        }
+
+        return newBoard;
+      });
+
+      setMoves((prevMoves) => prevMoves + 1);
+
+      if (moves < 50) {
+        setScore(1000);
+      } else if (moves < 100) {
+        setScore(750);
+      } else if (moves < 150) {
+        setScore(500);
+      } else if (moves < 200) {
+        setScore(250);
+      }
+    } else {
+      setMessage("Game over. Please start a new game.");
+    }
+  };
+
+  const finishGame = () => {
+    dispatch(upScore(score));
+    navigate("/endingScreen");
+  };
+
+  // Check if board is complete (no zeroes)
+  const boardComplete = () => {
+    return board.every((row) => row.every((cell) => cell === 1));
+  };
+
+  useEffect(() => {
+    if (!isInitialized) {
+      const easyBoard = [
+        [
+          [1, 0, 0, 0, 1],
+          [0, 1, 0, 1, 0],
+          [0, 0, 1, 0, 0],
+          [0, 1, 0, 1, 0],
+          [1, 0, 0, 0, 1],
+        ],
+        [
+          [0, 0, 1, 0, 0],
+          [0, 1, 0, 1, 0],
+          [1, 0, 1, 0, 1],
+          [0, 1, 0, 1, 0],
+          [0, 0, 1, 0, 0],
+        ],
+        [
+          [0, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, 0, 1, 0],
+          [0, 1, 1, 1, 0],
+          [0, 0, 0, 0, 0],
+        ],
+        [
+          [1, 0, 1, 0, 1],
+          [0, 1, 0, 1, 0],
+          [1, 0, 1, 0, 1],
+          [0, 1, 0, 1, 0],
+          [1, 0, 1, 0, 1],
+        ],
+        [
+          [0, 1, 0, 1, 0],
+          [1, 0, 0, 0, 1],
+          [0, 0, 1, 0, 0],
+          [1, 0, 0, 0, 1],
+          [0, 1, 0, 1, 0],
+        ],
+      ];
+
+      const randomIndex = Math.floor(Math.random() * easyBoard.length);
+      const randomBoard = easyBoard[randomIndex];
+
+      setBoard(randomBoard);
+      setDifficulty("easy");
+      setGameOver(false);
+      setMessage("");
+      setIsInitialized(true);
+    }
+  }, [isInitialized]);
+
+  useEffect(() => {
+    if (board) {
+      if (boardComplete()) {
+        setGameOver(true);
+        setMessage("You win!");
+      }
+    }
+  }, [board]);
+
+  return (
+    <div>
+      <div className="placementButton2">
+        <button
+          className="buttonAudio"
+          onClick={() => setAudioEnabled((prevEnabled) => !prevEnabled)}
+        >
+          {audioEnabled ? (
+            <img
+              src="https://www.freeiconspng.com/uploads/sound-off-button-icon-17.png"
+              alt="Disable Audio"
+              width="42px"
+            />
+          ) : (
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/17/17533.png"
+              alt="Enable Audio"
+              width="42px"
+            />
+          )}
+        </button>
+      </div>
+      {board && (
+        <>
+          <table>
+            <thead></thead>
+            <tbody>
+              {board.map((row, i) => (
+                <Row
+                  key={i}
+                  row={row}
+                  rowIndex={i}
+                  gameOver={gameOver}
+                  toggle={toggle}
+                  difficulty={difficulty}
+                />
+              ))}
+            </tbody>
+          </table>
+          {gameOver && (
+            <div className="buttonDiv">
+              <p className="message">Congratulations!</p>
+              <button className="escape" onClick={finishGame}>
+                Escape
+                <FontAwesomeIcon icon={faPersonRunning} />
+              </button>
+            </div>
+          )}
+        </>
+      )}
+
+      <p className="moves">Moves: {moves}</p>
+      <p className="dialogBox2">
+        The object of this game is to set the entire board to blue.
+      </p>
+    </div>
+  );
 };
 
 const Row = ({ row, rowIndex, gameOver, toggle, difficulty }) => {
-    return (
-        <tr>
-            {row.map((cell, i) => (
-                <Cell key={i} rowIndex={rowIndex} columnIndex={i} cellValue={cell} gameOver={gameOver} toggle={toggle} difficulty={difficulty} />
-            ))}
-        </tr>
-    );
+  return (
+    <tr>
+      {row.map((cell, i) => (
+        <Cell
+          key={i}
+          rowIndex={rowIndex}
+          columnIndex={i}
+          cellValue={cell}
+          gameOver={gameOver}
+          toggle={toggle}
+          difficulty={difficulty}
+        />
+      ))}
+    </tr>
+  );
 };
 
-const Cell = ({ rowIndex, columnIndex, cellValue, gameOver, toggle, difficulty }) => {
-    let light;
+const Cell = ({
+  rowIndex,
+  columnIndex,
+  cellValue,
+  gameOver,
+  toggle,
+  difficulty,
+}) => {
+  let light;
 
-    if (difficulty === 'easy') {
-        light = !gameOver ? (cellValue === 1 ? 'easy on' : 'easy off') : 'easy done';
-    } else if (difficulty === 'hard') {
-        light = !gameOver ? (cellValue === 1 ? 'hard on' : 'hard off') : 'hard done';
-    }
+  if (difficulty === "easy") {
+    light = !gameOver
+      ? cellValue === 1
+        ? "easy on"
+        : "easy off"
+      : "easy done";
+  } else if (difficulty === "hard") {
+    light = !gameOver
+      ? cellValue === 1
+        ? "hard on"
+        : "hard off"
+      : "hard done";
+  }
 
-    return (
-        <td>
-            <div className={light} onClick={() => toggle(rowIndex, columnIndex)}></div>
-        </td>
-    );
+  return (
+    <td>
+      <div
+        className={light}
+        onClick={() => toggle(rowIndex, columnIndex)}
+      ></div>
+    </td>
+  );
 };
 
 export default Board;
